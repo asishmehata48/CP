3a2cf6a65709702af0204b3ce9885ae3c3c971d2
Asish Mehata <mehataasish@gmail.com>
2025-08-25 15:00:00 +0545

    Data types in JS

---
+++ Javascript/2/typeconversion.js
@@ -0,0 +1,14 @@
+// Number + String -> String
+console.log(5 + "5"); // "55" (number converted to string)
+
+// Boolean + Number -> Number
+console.log(true + 2); // 3 (true = 1, false = 0)
+
+// Boolean + String -> String
+console.log(false + "hello"); // "falsehello"
+
+// Null with Number -> Number
+console.log(null + 5); // 5 (null becomes 0)
+
+// Undefined with Number -> NaN
+console.log(undefined + 5); // NaN
