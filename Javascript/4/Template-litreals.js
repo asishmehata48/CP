61b79361be7ff8efa9567a76d369c677058cba86
Asish Mehata <mehataasish@gmail.com>
2025-08-28 23:17:34 +0545

    Template Literals

---
+++ Javascript/4/Template-litreals.js
@@ -0,0 +1,15 @@
+let name = "Asish";
+let age = 21;
+
+// Normal String Concatenation
+console.log("Hello, my name is " + name + " and I am " + age + " years old.");
+
+// Using Template Literals
+console.log(`Hello, my name is ${name} and I am ${age} years old.`);
+
+// Multi-line String
+let msg = `
+This is a multi-line
+string using template literals.
+`;
+console.log(msg);
